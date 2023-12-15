9ea70556c3f59de2e703afcd84a35e647ebde791
G4BOs <108361351+G4BOs@users.noreply.github.com>
2023-12-15 15:45:54 -0300

    Delete inicio.js

--- inicio.js
+++
@@ -1,9 +0,0 @@
-const CERRARSESION = document.getElementById('cerrarsesion');
-
-
-CERRARSESION.addEventListener("click",cerrarsesion);
-
-function cerrarsesion(){
-    window.location.href='index.html';
-    return
-}
